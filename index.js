0a76ffd4f036ab280c4326bfdb87b84a2a432b65
thescarydoor <>
2024-03-27 01:03:06 -0700

    workflow change

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ console.log("hello");
 
 const sum = (a, b) => a + b;
 
+const sum2 = async (a, b) => a + b;
+
 module.exports = {
   sum,
+  sum2,
 };
--- index.test.js
+++ index.test.js
@@ -1,5 +1,6 @@
-const { sum } = require(".");
+const { sum, sum2 } = require(".");
 
-test("adds 1 + 2 to equal 3", () => {
+test("adds 1 + 2 to equal 3", async () => {
   expect(sum(1, 2)).toBe(3);
+  await expect(sum2(1, 2)).resolves.toBe(3);
 });
